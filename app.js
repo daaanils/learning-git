81daa882d8fb5273d0051399188fe9ef0188b53f
dannypelaso <danilobpelaso@iskolarngbayan.pup.edu.ph>
2023-09-29 19:55:50 +0800

    add features

--- app.js
+++ app.js
@@ -1,6 +1,119 @@
-function addition(a, b) {
-	return a + b;
 
-}
+// const DoSomething = (a, b) => {
+// 	return a + b
+// }
 
-console.log(addtion(3, 6));
+// console.log(DoSomething(12, 12))
+
+// if ('some string' === 'another string') {
+// 	console.log('the strings are equal')
+// }else {
+// 	console.log('the strings are not equal')
+// }
+
+// for (let i = 0; i < 100; i++) {
+// 	let i = 'danilo';
+// 	console.log(i)
+// }
+
+// const arr = ['Danilo','Jaycel', 'Vince', 'Mike', 'chano', 20, 23, 24, 50, 3, 3, true, false, ];
+
+// for (let i = 0; i < arr.length; i++) {
+
+// 	if (arr[i] === 'a') {
+// 		console.log(arr[i]);
+// 	}
+
+// }
+
+// function myFunction() {
+// 	return "hello world"
+// }
+
+// console.log(myFunction())
+
+// const arr1 = [0, 1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] > 2) {
+//         console.log(arr1[i]);
+//     }
+// }
+
+// let account = 100;
+// let lunch = 20;
+// let dinner = 50;
+// let earned = 200;
+
+// let totalMoneyLeft = account - (lunch + dinner) + earned;
+ 
+// console.log('$' + totalMoneyLeft);
+// 2095 + 799 + 1899
+
+//  let totalCost = Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100 + (2095 + 799 + 1899 + 499) / 100
+
+
+
+//  let hello = `items (${1 + 1}): $${(2095 + 799) / 100}`
+
+//  console.log(hello
+
+// let name = 'Danilo Pelaso Jr.'
+
+// let message = `My name is ${name}`;
+// let age = 10.5;
+// let isBoolean = true;
+
+// let person = {
+//     name: 'Danilo Pelaso',
+//     age : '22'
+// };
+
+// let names = ['Danilo', 'Jaycel', 'Vince'];
+
+// let add = function (a, b) {
+//     return a * b;
+// }
+
+// const add = (a, b) => a + b;
+
+// let x = 5;
+// let y = '5';
+
+
+// let isGreater = x === y;
+
+// let grade = 74;
+
+// if (grades >= 90) {
+//     mark = 'A';
+// }else if (grades >= 80) {
+//     mark = 'B';
+// }else if (grades >= 75) {
+//     mark = "C";
+// } else {
+//     mark = 'F';
+// }
+
+// const finalMark = grade >= 90 ? "A" : grade >= 80 ? "B" : grade >= 75 ? "C" : 'F';
+
+// const names = ['jaycel', 'vince', 'Mike'];
+// const [firstPerson, secondPerson, thirdPerson] = names;
+
+// const person = {
+//     name: 'Danilo Pelaso',
+//     age: 21,
+//     birthdate: new Date(),
+//     status: 'Single'
+// };
+
+// const {name, age, birthdate, status} = person;
+
+// ***Spread Operator***
+
+// const number = [1, 2, 3, 4, 5];
+// const completeNumber = [...number, 6, 7, 8, 9]
+
+
+
+// console.log(completeNumber);
